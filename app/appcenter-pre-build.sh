76975e87c27621c214e8f9610a367e93f4eaba12
Yiğit Can YILMAZ <yigitcany@nuevo.com.tr>
2020-06-19 14:43:25 +0300

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -6,8 +6,7 @@ echo "Updating build.gradle..."
 IdFile=$BUILD_REPOSITORY_LOCALPATH/app/build.gradle
 echo "IdFile=" $IdFile
 sed -i '' "s/APPCENTER_APP_ID/$APPCENTER_APP_ID/g" $IdFile
-sed -i '' "s/APPCENTER_KEYSTORE_PASSWORD/$APPCENTER_KEYSTORE_PASSWORD/g" $IdFile
-sed -i '' "s/APPCENTER_KEY_ALIAS/$APPCENTER_KEY_ALIAS/g" $IdFile
+
 echo "After string replace:"
 cat $IdFile
 echo "END Updating build.gradle"
