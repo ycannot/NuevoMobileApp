93915b544af0bc9f73998e72fc35aebd8d1186a4
Yiğit Can YILMAZ <yigitcany@nuevo.com.tr>
2020-06-19 14:10:53 +0300

    update

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,14 @@
 echo "Post build proccess has been started"
-./gradlew assembleDebug
-./gradlew assembleDebugAndroidTest
+#./gradlew assembleDebug
+$APPCENTER_SOURCE_DIRECTORY/gradlew assembleReleaseAndroidTest
+
+
+appcenter test run espresso \
+--app "Nuevo-Deneme/Nuevo-Deneme-App" \
+--devices "Nuevo-Deneme/android-test-set" \
+--app-path $APPCENTER_OUTPUT_DIRECTORY/app-release.apk \
+--test-series "denemetestseries" \
+--locale "en_US" \
+--build-dir $APPCENTER_SOURCE_DIRECTORY/app/build/outputs/apk/androidTest/release \
+--token $UITEST_TOKEN \
+--async \
